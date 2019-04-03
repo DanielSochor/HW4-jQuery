e2bfe77a35849cb9b1a1d8ac67b82c290d0eb5fd
Daniel Sochor <47047939+DanielSochor@users.noreply.github.com>
2019-04-03 14:57:46 -0500

    Add files via upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,29 +20,69 @@ var obiWanLifeReduction = 10
 
 $(document).ready(function () {
 
-    var playerCharacterChoosen = false;
-    var opponentChossen = false;
-    var battleOngoing = false;
+    var Obi_Wan = 120;
+    var Luke_Skywalker = 100;
+    var Darth_Sidious = 150;
+    var Darth_Maul = 180;
+    var chracterLife = {Obi_Wan,Luke_Skywalker,Darth_Sidious,Darth_Maul};
+    console.log(chracterLife);
+    var counter = 1;
+
+    var hasPlayerCharacterChoosen = false;
+    var isBattleOngoing = false;
+    //var choosenCharacter = 0;
+    //var choosenOpponent = 0;
+    var choosenCharacterStartingLife = 0;
+    var choosenOpponentStartingLife = 0;
+    var yourDamage = 0;
+    var computerDamage = 0;
+    var choosenCharacterRemainingLife = 0;
+    var choosenOpponentRemainingLife = 0;
 
     $(".character").on("click", function () {
-        if (playerCharacterChoosen == false) {
+        if (hasPlayerCharacterChoosen == false) {
             $(".character").not(this).appendTo("#middle-row").addClass("choose-opponent");
             $(this).addClass("choosen");
             $(".choosen").off();
-            playerCharacterChoosen = true;
-        } else if (battleOngoing == false) {
+            //choosenCharacter = this.value;
+            choosenCharacterStartingLife = chracterLife[$(this).attr("id")];
+            //console.log(choosenCharacterStartingLife);
+            hasPlayerCharacterChoosen = true;
+        } else if (isBattleOngoing == false) {
             $(this).appendTo("#bottom-row").addClass("selected-opponent");
-            battleOngoing = true;
+            choosenOpponent = this.value;
+            choosenOpponentStartingLife = chracterLife[$(this).attr("id")];
+            //console.log(choosenOpponentRemainingLife);
+            //console.log(choosenOpponent);
+            isBattleOngoing = true;
         }
     });
 
+    var choosenCharacterRemainingLife = choosenCharacterStartingLife;
+    var choosenOpponentRemainingLife = choosenOpponentStartingLife;
+
     $("#attack").on("click", function () {
+        console.log(choosenCharacterRemainingLife);
+        console.log(choosenOpponentRemainingLife);
+        yourDamage = 0.1 * choosenCharacterStartingLife * counter;
+        computerDamage = 0.1 * choosenOpponentStartingLife;
         battle();
     });
 
     function battle() {
-        console.log("hello");
-        battleOngoing = false;
+        if ((choosenCharacterRemainingLife > 0) && (choosenOpponentRemainingLife > 0)) {
+           choosenCharacterRemainingLife = choosenCharacterRemainingLife - computerDamage;
+           choosenOpponentRemainingLife = choosenOpponentRemainingLife - (counter * yourDamage);
+            console.log("game in play");
+            counter ++;
+            console.log(choosenCharacterRemainingLife);
+            console.log(choosenOpponentRemainingLife);
+        } else {
+            isBattleOngoing = false;
+        }
+
+        $("#your_attack").text("You attacked for " + yourDamage + " damage.");
+        $("#computer_attack").text("attacked you for " + computerDamage + " damage");
     }
 
     function lifeReduction() {
